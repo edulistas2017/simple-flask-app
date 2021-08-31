1a5e302e860c656c5ca96a1889507c8c64f323d4
edulistas2017 <30630362+edulistas2017@users.noreply.github.com>
2021-08-30 21:19:49 -0300

    adaptei para tentar abrir mapa

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, render_template
+app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    #return 'Hello, World!'
+    return render_template("mapa_analise_imoveis_zuckerman.html")
 
 if __name__ == '__main__':
     #app.run(debug=True,host='0.0.0.0')
